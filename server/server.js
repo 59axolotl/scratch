46d2a8cca3e2d82e3131e30b0c73b3d68e0949f4
Denton Wong <denton.wong@gmail.com>
2023-07-29 08:54:09 -0700

    messing with bootstrap

--- server/server.js
+++ server/server.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, '../dist')));
 
-
-
-
-
-
-
-
-
-app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+app.use((req, res) =>
+  res.status(404).send("This is not the page you're looking for...")
+);
 
 app.use((err, req, res) => {
   const defaultErr = {
@@ -32,5 +28,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
-
-
