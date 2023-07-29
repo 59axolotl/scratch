b968babb9e105c1f1cc4ed3ef4bdd8bbaa247d97
Denton Wong <denton.wong@gmail.com>
2023-07-29 08:54:09 -0700

    messing with bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '../components/App';
 
-console.log(document.getElementById('root'));
 const root = createRoot(document.getElementById('root'));
 
 root.render(<App />);
