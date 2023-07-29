5c8183abd135143a0013e769763b9daa2c4ccbe5
Danny Murcia <76888535+dm2800@users.noreply.github.com>
2023-07-29 14:10:10 -0700

    sets up backend/database

---
+++ server/config/jwt.config.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+
+module.exports = {
+    authenticate(req, res, next) {
+        jwt.verify(
+            req.cookies.usertoken,
+            process.env.JWT_SECRET,
+            (err, payload) => {
+                if (err) {
+                    console.log(err);
+                    res.status(401).json({ verified: false });
+                } else {
+                    console.log(payload);
+
+                    req.jwtpayload = payload;
+
+                    next();
+                }
+            }
+        );
+    },
+};
--- server/server.js
+++ server/server.js
@@ -1,30 +1,37 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 3000;
+const cookieParser = require("cookie-parser");
 
-app.use(express.static(path.join(__dirname, '../dist')));
+require("./config/mongoose.config");
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.use((req, res) =>
-  res.status(404).send("This is not the page you're looking for...")
+    res.status(404).send("This is not the page you're looking for...")
 );
 
 app.use((err, req, res) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+    const defaultErr = {
+        log: "Express error handler caught unknown middleware error",
+        status: 500,
+        message: { err: "An error occurred" },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
 });
 
 /**
  * start server
  */
 app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}...`);
+    console.log(`Server listening on port: ${PORT}...`);
 });
 
 module.exports = app;
