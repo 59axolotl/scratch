6b0fe9c40dc9a52265282d413e8773f1c1f2b85c
David Jones <vvisdomr@Davids-MacBook-Pro.local>
2023-07-30 04:24:27 -0500

    Optimize config files to conceal sensitive information - Move jwt.config.js JWT_SECRET to env - Move mongoose.config.js monguURI to .env

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,15 +1,11 @@
-const mongoose = require("mongoose");
-
-// creating variable for mongoDB url
-const mongoURI =
-    "mongodb+srv://vvisdomr:CxHYgxEp1AkUBUfz@cluster0.kjhoa47.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
 
 // connecting mongoose database to app
 mongoose
-    .connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: "axolotl_flix",
-    })
-    .then(() => console.log("Connected to MongoDB..."))
-    .catch((err) => console.log(err));
+  .connect(process.env.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'axolotl_flix',
+  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.log(err));
