7dad2aa2bfc060fd006964781fd9a80a23642216
Denton Wong <denton.wong@gmail.com>
2023-07-31 10:06:39 -0700

    make get route for useruploads video

--- server/controllers/VideoController.js
+++ server/controllers/VideoController.js
@@ -1,5 +1,5 @@
-const Video = require("../models/videoModel");
-const jwt = require("jsonwebtoken");
+const Video = require('../models/videoModel');
+const jwt = require('jsonwebtoken');
 
 const videoController = {
   
@@ -9,22 +9,39 @@ const videoController = {
 
     // //Keeping track of who created the listing: 
     const decodedJWT = jwt.decode(req.cookies.usertoken, {
-        complete: true,
+      complete: true,
     });
 
     newVideoObject.createdBy = decodedJWT.payload.id;
 
     await newVideoObject
-        .save()
-        .then((newVideo) => {
+      .save()
+      .then((newVideo) => {
             
-            res.locals.newVideoContent = newVideo;
-            return next();
-        })
-        .catch((err) => {
-          return next(err);
-        });
-},
+        res.locals.newVideoContent = newVideo;
+        return next();
+      })
+      .catch((err) => {
+        return next(err);
+      });
+  },
+
+  getCreatorUploads: async (req,res,next) => {
+
+    try {
+      const decodedJWT = jwt.decode(req.cookies.usertoken, { complete: true });
+      const creatorUpVideos = await Video.find({ createdBy: decodedJWT.payload.id }).populate({path: 'createdBy', select: '_id studio '});
+      res.locals.creatorUpVideos = creatorUpVideos;
+  
+      return next();
+    } catch (err) {
+      return next({
+        log: `videoController failed to find videos for logged in creator, ${err.message}.`,
+        status: 500,
+        message: { err: "Failed to get logged in creator." },
+      });
+    }
+  }
 };
 
 module.exports = videoController;
